0409fae8d4b92e576eb4622a18d164007a1aaf83
Jace Ju <jaceju@gmail.com>
2012-01-25 00:20:23 +0800

    Add Form style

--- js/script.js
+++ js/script.js
@@ -7,15 +7,27 @@ var init = function () {
   // Buttons
   $('.buttonset').buttonset();
 
+  // File Input
+  $('input[type=file]').customFileInput({
+    button_position : 'right'
+  });
+
+  // Date Picker
+  $('.datepicker').datepicker();
 };
 
 $(function () {
   // Menu
   $('#menu').load('menu.html', init);
 
-
   // Tabs
-  $('.tabs').tabs();
+  $('.tabs').tabs({
+    load: init
+  });
 
+  // Date Picker
+  $('.datepicker').datepicker();
+
+  // Button
   $('.button').button();
 });
